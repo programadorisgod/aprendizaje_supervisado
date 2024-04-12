4a8d1ae18063531a69187b48f7f63c9ddc54faca
programadorisgod <catapiascastro@gmail.com>
2024-04-12 18:38:52 -0500

    fix(post)

--- src/adapters/routes/backpropagation/route.py
+++ src/adapters/routes/backpropagation/route.py
@@ -9,7 +9,7 @@ router = APIRouter()
 
 
 def create_router_backpropagation(controller_backpropagation):
-    @router.get(BASE_URL, tags=['Backpropagation'])
+    @router.post(BASE_URL, tags=['Backpropagation'])
     async def get_weights_and_threshold(layerValues: LayerValues):
         return controller_backpropagation.get_weights_and_threshold(layerValues)
 
