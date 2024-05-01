fbbb126e890ebb4c353c4f542076154f35471b0b
programadorisgod <catapiascastro@gmail.com>
2024-04-30 21:10:36 -0500

    feat(BASE URL BP)

--- src/adapters/routes/backpropagation/route.py
+++ src/adapters/routes/backpropagation/route.py
@@ -4,7 +4,7 @@ from src.entity.models.patron import LayerValues
 from src.entity.models.patron import Configuration
 
 
-BASE_URL = '/api/v1/backpropagation'
+BASE_URL = '/api/v1/backpropagation/values'
 
 router = APIRouter()
 
@@ -13,7 +13,7 @@ def create_router_backpropagation(controller_backpropagation):
     @router.post(BASE_URL, tags=['Backpropagation'])
     async def get_weights_and_threshold(layerValues: LayerValues):
         return controller_backpropagation.get_weights_and_threshold(layerValues)
-    
+
     @router.post(f"{BASE_URL}/config", tags=['Backpropagation'])
     async def get_neural_configuration(configuration: Configuration):
         return controller_backpropagation.get_neural_configuration(configuration)
