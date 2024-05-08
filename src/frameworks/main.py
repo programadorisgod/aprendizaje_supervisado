16a03bfd30e21dd1d477a6fbff88bc691db8a924
programadorisgod <catapiascastro@gmail.com>
2024-05-08 08:42:46 -0500

    fix(uvicornx)

--- src/frameworks/main.py
+++ src/frameworks/main.py
@@ -42,7 +42,7 @@ controller_backpropagation = Controller_backpropagation(
 app.include_router(create_router_error_correction(controller_error_correction))
 app.include_router(create_router_backpropagation(controller_backpropagation))
 
-""" if __name__ == "__main__":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run("src.frameworks.main:app",
-                host='0.0.0.0', port=8000, reload=True) """
+                host='0.0.0.0', port=8000, reload=True)
