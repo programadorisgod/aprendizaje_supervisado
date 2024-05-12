cf3ae32d32450d5a2fe8f4ac1529648c1b3ed75e
MSLegro <legromanuel29@gmail.com>
2024-05-12 00:05:49 -0500

    Lectura de entradas, salidas y patrones de un el dataset con letras

--- src/adapters/utils/counting_input_output_and_patterns.py
+++ src/adapters/utils/counting_input_output_and_patterns.py
@@ -10,16 +10,21 @@ def counting_input_output_and_patterns(patrons: list[Patron]) -> list:
     current_letter = None
     first_patron = patrons[0]
 
+    
+
     for key, value in first_patron.items():
         if key.lower() != '_id':
             headers.append(key.lower())
 
+    if headers[0] != 'x1':
+        headers[0] = 'x1'
+        headers[1] = 'x2'
+        headers[2] = 'x3'
+        headers[3] = 'yd1'
+
+    
     for header in headers:
-        """
-        las entradas, tienen la misma letra en la primera posición, las salidas no.
-        Por ende si la primera letra de la entrada es igual a la letra actual, se incrementa m, sino se incrementa n.
-        ya que iniciaria current_letter en None, se le asigna la primera letra de la primera entrada. en la 2iteracion ya current_letter no es None y se compara con la primera letra de la entrada actual. y eso es basicamente en ese for
-        """
+
         if current_letter is None or header[0] == current_letter:
             current_letter = header[0]
             m += 1
@@ -27,4 +32,6 @@ def counting_input_output_and_patterns(patrons: list[Patron]) -> list:
             n += 1
 
     input_output_patrons.append([m, n, len(patrons)])
+        
+        
     return input_output_patrons
