26753c17aa86d2e5f05d7d0f2e9438af308d19fe
MSLegro <legromanuel29@gmail.com>
2024-05-13 15:53:23 -0500

    Ya jesu, ya

--- src/adapters/utils/back_propaging/refactor_data_set.py
+++ src/adapters/utils/back_propaging/refactor_data_set.py
@@ -6,20 +6,13 @@ def refactor_data_set(patrons: list[Patron]):
     current_group = []
     all_groups = []
     binary_groups = []
+    count = 0
 
     result_to_binary = {
         'blandas': 1,
-        'duras': 0
-    }
-
-    patrons_to_binary = {
-        'si': 1,
-        'no': 0,
-        'joven': 0,
-        'pre-presbicia':1,
-        'presbicia': 2,
-        'miopia': 1,
-        'hipermetropia': 0,
+        'duras': 0,
+        'concedido': 1,
+        'denegado': 0
     }
 
     for patron in patrons:
@@ -30,9 +23,27 @@ def refactor_data_set(patrons: list[Patron]):
                     all_groups.append(current_group)
                     current_group = [] 
 
-
     for group in all_groups:
-        inputs = [patrons_to_binary.get(group[0]),patrons_to_binary.get(group[1]),patrons_to_binary.get(group[2])]
-        binary_groups.append([inputs, [ result_to_binary.get(group[3], 0)] ])
+        value = bin(count)[2:].zfill(3)
+        ouputs = [ result_to_binary.get(group[3], 0)]
 
+        if len(value) != 4:
+            inputs = [int(digit) for digit in value]
+        else:
+            address = find_address(all_groups, group[3])
+
+            value = bin(address[0])[2:].zfill(3)
+            inputs = [int(digit) for digit in value]
+
+        binary_groups.append([inputs, ouputs ])
+        count += 1
     return binary_groups
+
+
+def find_address(matriz, valor):
+    for fila in range(len(matriz)):
+        for columna in range(len(matriz[fila])):
+            if matriz[fila][columna] == valor:
+                return fila, columna
+    return None
+
